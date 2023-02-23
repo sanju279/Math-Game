e8bb0fd9dbfcc436ae0dc2417a8acb086d62eb5b
Sanju279 <117310661+sanju279@users.noreply.github.com>
2023-02-23 17:12:53 +0000

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -5,8 +5,8 @@ function addUser() {
 
     //Set "player1_name" using localStorage.setItem() function
 	  //Set "player2_name" using localStorage.setItem() function
-    localStorage.setItem("player1_name", addUser);
-    localStorage.setItem("player2_name", addUser);
+    localStorage.setItem("player1_name", player1__name);
+    localStorage.setItem("player2_name", player2_name);
 
     window.location = "game_page.html";
 }
